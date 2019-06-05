a8105695ff8111d8866f6ac803a0a904e7af689e
Ulf Lilleengen <lulf@redhat.com>
2019-06-05 15:56:57 +0200

    Add svelte version of app

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import client from 'rhea';
+	export let name;
+  var server = "ws://127.0.0.1:8080";
+  client.on("message", function (context) {
+      console.log("Got message?");
+  });
+  client.on("connection_open", function (context) {
+      console.log("Connected!");
+  });
+  var ws = client.websocket_connect(WebSocket);
+  console.log("Connecting");
+  client.options.username = "test";
+  client.options.password = "test";
+  var connection = client.connect({"connection_details":ws(server, ["binary", "AMQPWSB10", "amqp"]), "reconnect":false});
+  console.log("Opening receiver");
+  connection.open_receiver("events");
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
