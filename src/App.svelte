d65563c2fb70f6e55f7bc8ca326bb44c86bd7885
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2019-12-18 16:36:46 +0100

    Fix

--- src/App.svelte
+++ src/App.svelte
@@ -7,12 +7,11 @@
     uri: 'http://api.teig.cloud:8080/graphql'
   });
 
-  console.log("Creating graphql sub");
   var sensorTypes = ["motion", "temperature"];
   var deviceData = new Map();
   var deviceInfo = new Map();
   var charts = {};
-  var window = 24 * 60 * 60; // 24 hours
+  var window = 1 * 30 * 60; // 24 hours
   const deviceQuery = gql`query Query {
       devices {
         id
@@ -31,7 +30,6 @@
       var since = now - window; 
       for (var idx in data.devices) {
         var device = data.devices[idx];
-        console.log("Id: " + device.id);
         if (deviceData.get(device.id) === undefined) {
           var sensorData = new Map();
           for (var sensorIdx in device.sensors) {
@@ -43,6 +41,7 @@
           deviceInfo.set(device.id, device);
         }
         
+        deviceInfo = deviceInfo;
         const eventObservable = client.watchQuery({
           query: gql`query Query {
             events (deviceId: "${device.id}", since: ${since}) {
@@ -58,15 +57,23 @@
         eventObservable.subscribe({
           next: ({data}) => {
             var events = data.events
+            var entries = {};
             for (var eidx in events) {
               var event = events[eidx];
               for (var dataKey in event.data) {
                 if (deviceData.get(device.id).get(dataKey) !== undefined) {
-                  var entry = {timestamp: event.creationTime, value: event.data[dataKey]};
-                  deviceData.get(device.id).get(dataKey).push(entry);
+                  var entry = {timestamp: event.creationTime};
+                  entry[dataKey] = event.data[dataKey];
+                  if (entries[dataKey] === undefined) {
+                    entries[dataKey] = [];
+                  }
+                  entries[dataKey].push(entry);
                 }
               }
-            }  
+            }
+            for (var dataKey in entries) {
+              deviceData.get(device.id).set(dataKey, entries[dataKey]);
+            }
             updateChart(device.id);
           }
         });
@@ -74,117 +81,123 @@
     }
   });
 
+  var motionChart = function (options, dataset, sensorData) {
+    dataset.label = 'Motion';
+    dataset.data = sensorData.map(function (v) { return v.motion === true ? 1 : 0; });
+    dataset.steppedLine = true;
+    options.elements = {
+      point: {
+        radius: 0
+      }
+    };
+    options.scales.yAxes = [
+      {
+        ticks: {
+          padding: 5,
+          display: false,
+          beginAtZero: true,
+          min: 0,
+          max: 1.5,
+        },
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: "Motion"
+        }
+      }
+    ];
+  };
+
   var updateChart = function (deviceId) {
     var now = Math.floor(Date.now() / 1000);
     var since = now - window; 
     var sensors = deviceData.get(deviceId);
     if (sensors !== undefined) {
       for (var [sensorId, sensorData] of sensors) {
-        /*var date = new Date(sensorData.timestamp);
-        var formatted_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();*/
-
         var chartName = "chart_" + deviceId + "_" + sensorId;
-        if (charts[chartName] === undefined) {
-          var element = document.getElementById(chartName);
-          if (element != null) {
-            var ctx = element.getContext('2d');
-            const cdata = {
-              labels: sensorData.map(function (v) { return new Date(v.timestamp * 1000); }),
-              datasets: [
-                {
-                  fill: false,
-                  label: 'Motion',
-                  data: sensorData
-                    // .filter(function (v) { return v.value === true; })
-                    .map(function (v) { return v.value === true ? 1 : 0; }),
-                  borderColor: '#fe8b36',
-                  backgroundColor: '#fe8b36',
-                  lineTension: 0,
-                  steppedLine: true,
-                }
-              ]
-            };
-            const options = {
-              elements: {
-                point: {
-                  radius: 0
-                }
-              },
-              legend: {
-                display: false
-              },
-              fill: false,
-              responsive: true,
-              scales: {
-                bounds: 'ticks',
-                xAxes: [
-                  {
-                    type: 'time',
-                    display: true,
-                    distribution: 'linear',
-                    source: 'labels',
-                    bounds: 'ticks',
-                    ticks: {
-                      bounds: 'ticks',
-                    },
-                    scaleLabel: {
-                      display: false,
-                      labelString: "Date"
-                    }
-                  }
-                ],
-                yAxes: [
-                  {
-                    ticks: {
-                      padding: 5,
-                      display: false,
-                      beginAtZero: true,
-                      min: 0,
-                      max: 1.5,
-                    },
-                    display: true,
-                    scaleLabel: {
-                      display: true,
-                      labelString: "Motion"
-                    }
-                  }
-                ]
+        var element = document.getElementById(chartName);
+        sensorData = sensorData.filter(function (v) {
+          return v.timestamp >= since;
+        });
+        if (element != null) {
+          var cdata = {
+            labels: sensorData.map(function (v) { return new Date(v.timestamp * 1000); }),
+            datasets: [
+              {
+                fill: false,
+                borderColor: '#fe8b36',
+                backgroundColor: '#fe8b36',
+                lineTension: 0,
               }
-            };
+            ]
+          };
+          var options = {
+            animation: false,
+            legend: {
+              display: false
+            },
+            fill: false,
+            responsive: true,
+            scales: {
+              bounds: 'ticks',
+              xAxes: [
+                {
+                  type: 'time',
+                  display: true,
+                  distribution: 'linear',
+                  source: 'labels',
+                  bounds: 'ticks',
+                  ticks: {
+                    bounds: 'ticks',
+                  },
+                  scaleLabel: {
+                    display: false,
+                    labelString: "Date"
+                  }
+                }
+              ],
+            }
+          };
+          if (sensorId == "motion") {
+            motionChart(options, cdata.datasets[0], sensorData);
+          }
+
+          console.log("Cdata (data: " + cdata.datasets[0].data.length + ", labels: " + cdata.labels.length + ": " + JSON.stringify(cdata.datasets[0].data));
+          var ctx = element.getContext('2d');
+          if (charts[chartName] === undefined) {
+            console.log("Creating new chart " + chartName);
             var chart = new Chart(ctx, {
               type: 'line',
               data: cdata,
               options: options
             });
             charts[chartName] = chart;
+          } else {
+            console.log("Updating chart " + chartName);
+            chart = charts[chartName];
+            chart.data = cdata;
+            chart.options = options;
+            chart.update();
           }
-        }/* else {
-          chart = charts[chartName];
-          chart.data.labels.push(value.date);
-          chart.data.datasets.forEach((dataset) => {
-            dataset.data.push(value.data);
-           });
-           chart.update();
         }
-        */
       }
     }
   };
 
   var updateState = function () {
-  /*
     for (var [deviceId, sensors] of deviceData) {
+        updateChart(deviceId);
+        /*
         for (var [sensorId, sensorData] of sensors) {
             console.log("Data for " + deviceId + " sensor " + sensorId + ": " + JSON.stringify(sensorData));
         }
+        */
     }
-    */
-    deviceData = deviceData;
     deviceInfo = deviceInfo;
-    setTimeout(updateState, 5000);
+    setTimeout(updateState, 30000);
   };
+  setTimeout(updateState, 30000);
 
-  setTimeout(updateState, 0);
 </script>
 
 <style>
@@ -205,12 +218,12 @@
 <th>Dings</th>
 <th colspan="{sensorTypes.length}">Historikk</th>
 </tr>
-{#each Array.from(deviceData.keys()) as device}
+{#each Array.from(deviceInfo.keys()) as device}
 <tr>
 <td>
 {deviceInfo.get(device).description}
 </td>
-{#each Array.from(deviceData.get(device).keys()) as sensor}
+{#each Array.from(deviceInfo.get(device).sensors) as sensor}
 <td>
 <canvas id="chart_{device}_{sensor}" width="800" height="200"></canvas>
 </td>
