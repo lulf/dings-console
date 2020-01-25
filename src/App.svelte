068762f585740dcdff715c2812af2181e117f425
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-01-25 20:44:34 +0100

    Update slider css

--- src/App.svelte
+++ src/App.svelte
@@ -402,8 +402,39 @@
     width: 45%;
     height: 300px;
   }
-  .slider {
+  .slidecontainer {
     width: 100%;
+  }
+
+  .slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #d3d3d3;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+  }
+  
+  .slider:hover {
+    opacity: 1;
+  }
+  
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    background: #fe8b36;
+    cursor: pointer;
+  }
+  
+  .slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    background: #fe8b36;
+    cursor: pointer;
   }
 </style>
 
@@ -414,12 +445,12 @@ Last updated: {maxDate}
 </p>
 
 <p>
-Events since: {dateSince}
+Events since (adjust slider to change start date): {dateSince}
 </p>
 
 <!--
 <div id='range' class="slider"></div> -->
-<div>
+<div class="slidecontainer">
 <input id="windowrange" type="range" class="slider" value="{window}" step="3600" min="0" max="{maxWindow}" on:change="{() => rangeUpdated()}" bind:value={window} />
 </div>
 
